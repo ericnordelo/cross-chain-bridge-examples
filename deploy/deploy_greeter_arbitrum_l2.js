861f1b64acc438fdd8425bc6c7f0d7a09d428cc1
Eric Nordelo <eric.nordelo39@gmail.com>
2022-06-21 00:51:58 +0200

    refactor: remove unnecessary comments

--- deploy/deploy_greeter_arbitrum_l2.js
+++ deploy/deploy_greeter_arbitrum_l2.js
@@ -2,7 +2,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  // this contract is upgradeable through uups (EIP-1822)
   await deploy('GreeterArbitrumL2', {
     from: deployer,
     log: true,
